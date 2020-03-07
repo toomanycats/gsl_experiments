89ebcc05d334af3259de49cddf9ca04810bf52f3
Daniel Cuneo <dpcuneo@fastmail.fm>
2020-03-07 13:52:39 -0800

    name changed

---
+++ convert_ascii_data_to_binary.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/*
+ * Issue with ofstream open not accepting a C++ string.
+ * https://stackoverflow.com/questions/22567256/how-to-fix-no-matching-function-ifstream-error
+ */
+
+class ConvertData
+{
+    public:
+        void load_data_from_file(string infile, vector<string> &rows)
+		{
+			int count;
+			ifstream fin;
+			string line;
+
+			fin.open(infile.c_str(), ios::in);
+			while (fin >> line) {
+				//rows.clear();
+				getline(fin, line);
+				rows.push_back(line);
+				count++;
+			}
+			cout << count << " lines counted." << endl;
+		}
+
+        void write_rows_to_binary(string outfile, vector<string> &rows){
+            vector<string>::iterator it;
+
+            ofstream fout;
+            fout.open(outfile.c_str(), fstream::binary);
+
+            for(it = rows.begin(); it != rows.end(); it++) {
+                fout << (char*) &it;
+            }
+
+            fout.close();
+        }
+};
+
+int main()
+{
+    string input_data_path = "data.csv";
+    string output_data_path = "data.bin";
+    vector<string> rows;
+
+    ConvertData convert_data;
+
+    convert_data.load_data_from_file(input_data_path, rows);
+    convert_data.write_rows_to_binary(output_data_path, rows);
+}
